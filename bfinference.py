e14298d5a5232955c59b29892e93e8ce54e88b3e
Adrian Gordon <adriangordon60@gmail.com>
2019-07-04 12:25:17 +0100

    initial commit

---
+++ bfinference.py
@@ -0,0 +1,113 @@
+#basic seqtoseq inference model for time series
+#usage: python basicmodelinference.py <configfilename>
+
+import tensorflow as tf
+
+import os
+
+import sys
+
+import json
+
+from matplotlib import pyplot as plt
+
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+
+import pandas as pd
+import numpy as np
+
+
+from basicmodel import *
+
+configfilename = sys.argv[1]
+
+with open(configfilename,'r') as f:
+  config = json.load(f)
+
+generate_data_library = config["data_modulename"]
+
+cmd = 'from ' + generate_data_library + ' import *'
+
+exec(cmd)
+
+ #now do some inference
+
+#create a seq2seq instance
+seq2seqInference  = Seq2Seq()
+init = tf.global_variables_initializer()
+
+gd=GenerateData(config["datafilename"])
+
+n_rows = len(GenerateData.processed_data) / 30
+
+
+column_increment = 0
+
+limit = 0.01
+
+scaler = joblib.load("scaler.save")
+
+with tf.Session() as sess:
+  sess.run(init)
+  saver = tf.train.Saver
+  saver().restore(sess, os.path.join('./',config["savefilename"]))
+
+  for row in range(1,n_rows):
+    print("NEXT ROW", row)
+    for column in range(1):
+      print("n_rows:", n_rows,"ROW:", row, "NEXT COLUMN: ", column)
+      test_sequence_input = gd.getTestSample(30, 10, row, column)
+      test_sequence_input_1 = gd.getTestSample(30, 15, row, column)
+
+      print(test_sequence_input.transpose())
+
+      feed_dict={encoder_inputs[t]: test_sequence_input[t].reshape(1,config["input_dim"]) for t in range(config["input_sequence_length"])}
+      feed_dict.update({decoder_target_inputs[t]: np.zeros([1,config["output_dim"]]) for t in range(config["output_sequence_length"])})
+      #print(feed_dict)
+      test_out = np.array(sess.run(seq2seqInference.encoder_decoder_inference,feed_dict)).transpose()#.reshape(-1)[:20]
+      test_lay_output = test_out[0].reshape(-1)
+      #test_output = np.array(test_out).reshape(-1)
+      #print(test_lay_output)
+      test_back_output = test_out[1].reshape(-1)
+      #print(test_back_output)
+      last_observed = test_sequence_input[9][0]
+      next_predicted = test_lay_output[0]
+
+      #print(last_observed, next_predicted, last_observed - next_predicted)
+
+      #if abs(last_observed - next_predicted) < limit :
+
+      #transposed = test_sequence_input.transpose()
+      #print(transposed)
+      
+      observed_ar= np.zeros((15,40))
+      for row1 in range(15):
+        observed_ar[row1][0] = test_sequence_input_1[row1][0]
+        observed_ar[row1][20] = test_sequence_input_1[row1][2]
+      descaled_observed_ar = scaler.inverse_transform(observed_ar)
+      #print(descaled_observed_ar[:,0])
+      #print(descaled_observed_ar[:,20])
+
+      predicted_ar = np.zeros((5, 40))
+      for row2 in range(5):
+        predicted_ar[row2][0]= test_lay_output[row2]
+        predicted_ar[row2][20] =test_lay_output[row2]
+
+      descaled_predicted_ar = scaler.inverse_transform(predicted_ar)
+
+
+
+      plt.figure(figsize=(15,4))
+      l1, = plt.plot(descaled_observed_ar[:,0], 'b.', label = 'Actual lay')
+      l2, = plt.plot(descaled_observed_ar[:,20], 'y.', label = 'Actual back')
+      l3, = plt.plot(range(10,15),descaled_predicted_ar[:,0], 'r.', label = 'Predicted lay')
+      l4, = plt.plot(range(10,15),descaled_predicted_ar[:,20], 'g.', label = 'Predicted back')
+
+      plt.legend(handles=[l1, l2, l3, l4], loc='upper left')
+
+      plt.show()
+      
+      i = raw_input("")
+
---
+++ generate_bf_data.py
@@ -0,0 +1,90 @@
+#Generate data samples from an input file - assumed to have been preprocessed (standardised, etc) - as is done in 'preprocess_bf_data.py'
+
+import tensorflow as tf
+import numpy as np
+
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+
+class GenerateData:
+  data =[]
+  processed_data =pd.DataFrame()
+  def __init__(self, datafile_path):
+    GenerateData.processed_data = pd.read_csv(datafile_path)
+    
+
+  def getTrainingSample(self,seq_length,batch_size, input_seq_length, output_seq_length):
+    data = GenerateData.processed_data
+    _nsequences = len(data) / seq_length
+    input_batches = []
+    output_batches = []
+
+    for i in range(batch_size):
+      #n_sequences = len(data) / (input_seq_length + output_seq_length)
+      sequence_index = np.random.randint(0,_nsequences)
+      sequence_index = 0
+     # print(sequence_index)
+      n_starting_indexes = seq_length - (input_seq_length + output_seq_length) -1
+      #print(n_starting_indexes)
+      starting_index = np.random.randint(0,n_starting_indexes)
+      starting_index = 0
+      #print(starting_index)
+      starting_offset = sequence_index * seq_length
+      #print(starting_offset)
+      an_input_sequence = data[starting_offset + starting_index: starting_offset + starting_index + input_seq_length]
+      #print(an_input_sequence)
+
+      input_data = an_input_sequence[['layprice1','laydepth1','backprice1','backdepth1']]
+      input_batches.append(np.array(input_data))
+
+      an_output_sequence = data[starting_offset + starting_index + input_seq_length:starting_offset +starting_index + input_seq_length + output_seq_length]
+      #print(an_output_sequence)
+      output_data = an_output_sequence[['layprice1','backprice1']]
+      output_batches.append(np.array(output_data))
+
+
+    return input_batches, output_batches
+
+  def getTestSample(self, seq_length, input_seq_length, offset1, offset2):
+    '''seq_length - length of the underlying sequence
+        input_seq_length - length of input sequence to generate
+        offset1 - offset into sequence
+        offset2 - offset within sequence
+    '''
+    input_batches = []
+    print("seq_length: ", seq_length, " input_seq_length: ", input_seq_length, " offset1: ", offset1, "offeset2: ", offset2)
+    start_index = (offset1 * seq_length) + offset2
+    end_index = start_index + input_seq_length
+    print("start:", start_index, "end: ", end_index)
+    an_input_sequence = GenerateData.processed_data[start_index:end_index]
+    input_data = an_input_sequence[['layprice1','laydepth1','backprice1','backdepth1']]
+
+    return np.array(input_data)
+    #input_batches.append(np.array(input_data))
+    #return input_batches
+
+
+  def reshape(self, input_array, sequence_length, input_dimension):
+    reshaped = [None]* sequence_length 
+
+    for t in range(sequence_length):
+      x = input_array[:,t].reshape(-1,input_dimension)
+      reshaped[t]=x
+    return(np.array(reshaped))
+
+#test
+#gd = GenerateData('./data/preprocessed_generate.csv')
+#print("processed data: ")
+#print(gd.processed_data)
+#print(gd.data[0:60])
+#input_batch, output_batch = gd.getTrainingSample(30,1,10,5)
+#print(input_batch)
+#print(output_batch)
+
+#reshaped_input_batch = gd.reshape(np.array(input_batch),30, 40)
+#print(reshaped_input_batch)
+
+#reshaped_output_batch = gd.reshape(np.array(output_batch), 10, 2)
+#print(reshaped_output_batch)
